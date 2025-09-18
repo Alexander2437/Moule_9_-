58dceac5cfa65b846ad7a93f0666107740f67934
Alexander2437 <alexander.vtyurin@gmail.com>
2025-09-18 18:51:31 +0700

    Задание 1 и  задание 2 (без try...catch)

--- Moule_9_Практикум_App/Program.cs
+++ Moule_9_Практикум_App/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Moule_9_Практикум_App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Module_9_Практикум_App
 {
@@ -6,7 +9,86 @@ namespace Module_9_Практикум_App
     {
         public static void Main(string[] args)
         {
+            // Задание 1
 
+            //Exception[] exceptions = new Exception[]
+            //{
+            //    new ArgumentNullException("param", "Аргумент не может быть null"),
+            //    new InvalidOperationException("Неверная операция"),
+            //    new NewException("Мое собственное исключение"),
+            //    new DivideByZeroException("Деление на ноль"),
+            //    new FormatException("Неверный формат")
+            //};
+
+            //foreach (var ex in exceptions)
+            //{
+            //    try
+            //    {
+            //        throw ex;
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine("Поймано исключение: " + e.Message);
+            //    }
+            //}
+
+            // Задание 2
+
+            List<string> surnames = new List<string>() { "Иванов", "Смирнов", "Петров", "Дуров", "Юсупов"};
+
+            Console.WriteLine("Начальный список фамилий:");
+            PrintList(surnames);
+
+            SortHelper sorter = new SortHelper();
+            sorter.SortEvent += (list, asc) =>
+            {
+                if (asc)
+                {
+                    list.Sort();
+                }
+                else
+                {
+                    list.Sort();
+                    list.Reverse();
+                }
+            };
+
+            bool condition;
+            do
+            {
+                Console.WriteLine("Введите цифрами \"1\" для сортировки списка от А до Я или \"2\" для сортировки от Я до А.");
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    sorter.TriggerSort(surnames, true);
+                    Console.WriteLine("Cписок отсортирован от А до Я.");
+                    PrintList(surnames);
+                    condition = true;
+                }
+                else if (input == "2")
+                {
+                    sorter.TriggerSort(surnames, false);
+                    Console.WriteLine("Cписок отсортирован от Я до А.");
+                    PrintList(surnames);
+                    condition = true;
+                }
+                else
+                {
+                    condition = false;
+                    continue;
+                }
+            }while (condition == false);
+        }
+
+        public static void PrintList(List<string> surnameList)
+        {
+            foreach (string surname in surnameList)
+            {
+                {
+                    Console.WriteLine(surname);
+                }
+            }
         }
     }
 }
---
+++ Moule_9_Практикум_App/SortHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moule_9_Практикум_App
+{
+    public class SortHelper
+    {
+        public delegate void SortEventHandler(List<string> surnames, bool ascending);
+        public event SortEventHandler SortEvent;
+
+        public void TriggerSort(List<string> sernames, bool ascending)
+        {
+            SortEvent?.Invoke(sernames, ascending);
+        }
+    }
+}
